4fd7cd64d5f08d1d2bebc111ab181edcef85f3aa
Sara <sara@sbyoung.de>
2009-08-12 21:54:03 -0400

    changed welcome box to add summer and winter service time

--- coshome.php
+++ coshome.php
@@ -13,6 +13,8 @@
 
 <p>This website is intended as a window into the community that is Church of Our Saviour. You will find information about our services and programs, about our activities for children, and our efforts to reach out to others. Beyond that you will read stories of our life together as a spiritual community that tells you a bit about who we are.</p>
 
-<p>What makes us really want to be here, however, lies beyond these pages in the warmth and care of the people who make this their church. We hope you will join us on Sunday morning at 9:00 AM, and consider making Church of Our Saviour your spiritual home.</p></div>
+<p>What makes us really want to be here, however, lies beyond these pages in the warmth and care of the people who make this their church. We hope you will join us on Sunday morning, and consider making Church of Our Saviour your spiritual home.</p>
+
+<p>Holy Eucharist is at 9 a.m. during the summer and at 10 a.m. beginning September 13.</p></div>
 </div>
 
